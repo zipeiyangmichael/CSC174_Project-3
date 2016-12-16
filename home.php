41d4fe005fea3061f1794079a3e30012dea342d6
zipeiyangmichael <zipeiyangmichael@dhcp-10-5-1-149.wireless.rochester.edu>
2016-12-15 22:18:52 -0500

    Home Page_v1
    Rough draft of Home Page

---
+++ home.php
@@ -0,0 +1,71 @@
+<?php include "inc/topPart.inc"; ?>
+
+<?php include "inc/nav.inc"; ?>
+
+<div class="parallax-container valign-wrapper">
+      <div class="parallax"><img src="images/background1.png"></div>
+</div>
+
+<div class="row home-s1">
+	<div class="col l4 s12">
+		<h1>ZIPEI</h1>
+	</div>
+	<div class="home-s1-h1 col l4 s12">
+		<h1>Mike</h1>
+	</div>
+	<div class="col l4 s12">
+		<h1>YANG</h1>
+	</div>
+</div><!--.row-->
+
+<div class="parallax-container valign-wrapper">
+   <div class="parallax"><img src="images/background2.jpg"></div>
+</div>
+
+<div class="row home-s2">
+<h2>About Me</h2>
+
+<article class="col l8 s12">
+<h3>Education</h3>
+<p>Zipei was born in Shenzhen, China and grew up there until gratuated from Senior High School. He was admitted to University of Rochester and is now studying abroad in America. Zipei is now a Sophomore.</p>
+<div class="home-s2-div">Education History</div>
+<ul>
+	<li>Shenzhen Experimental Kindergarten</li>
+	<li>Shenzhen Experimental Primary School</li>
+	<li>Shenzhen Experimental Middle School</li>
+	<li>Shenzhen Middle School</li>
+	<li>University of Rochester</li>
+</ul>
+<h3>Swimming</h3>
+<p>Zipei's father used to won the Asian Game so he started trainning Zipei to be a swimmer when he was 4 years old. Even though Zipei doesn't become a professional athletic, he has surely become one of the most competitive swimmer in his city.</p>
+<div class="home-s2-div">Swimming Awards</div>
+<ul>
+	<li>Guangdong Provience-wide Swimming Competition Youth Group 1st</li>
+	<li>Shenzhen District-wide Swimming Competition Youth Group 1st</li>
+	<li>Shenzhen District-wide Swimming Competition Kid Group 1st</li>
+	<li>Shenzhen City-wide Swimming Competition Youth Group 1st</li>
+	<li>Shenzhen City-wide Swimming Competition Kid Group 1st</li>
+	<li>Others</li>
+</ul>
+
+</article>
+
+<aside class="col l4 s12">
+<h3>Street-Dancing</h3>
+<p>Zipei started street-dancing when he was 9th grade.</p>
+<div class="home-s2-div">Dancing Team</div>
+<ul>
+	<li>SZSY Shadow Dancing Crew</li>
+	<li>SMS Nightfury Crew</li>
+	<li>Wabble-Street</li>
+</ul>
+</aside>
+
+
+
+
+
+<?php include "inc/scripts.inc" ?>
+
+ </body>
+</html>
