69f657297bfbc665f76904026806b9f567d76bdf
zipeiyangmichael <zipeiyangmichael@dhcp-10-5-1-149.wireless.rochester.edu>
2016-12-16 00:19:38 -0500

    Portfolio Page_v1
    Rough Draft of Portfolio Page, there might be an existing bug: the
    “slideshow” plugin doesn’t always work, sometimes disappear after
    refreshing the page.

--- home.php
+++ home.php
@@ -28,6 +28,7 @@
 <article class="col l8 s12">
 <h3>Education</h3>
 <p>Zipei was born in Shenzhen, China and grew up there until gratuated from Senior High School. He was admitted to University of Rochester and is now studying abroad in America. Zipei is now a Sophomore.</p>
+<div  class="home-s2-li">
 <div class="home-s2-div">Education History</div>
 <ul>
 	<li>Shenzhen Experimental Kindergarten</li>
@@ -36,8 +37,10 @@
 	<li>Shenzhen Middle School</li>
 	<li>University of Rochester</li>
 </ul>
+</div>
 <h3>Swimming</h3>
 <p>Zipei's father used to won the Asian Game so he started trainning Zipei to be a swimmer when he was 4 years old. Even though Zipei doesn't become a professional athletic, he has surely become one of the most competitive swimmer in his city.</p>
+<div  class="home-s2-li">
 <div class="home-s2-div">Swimming Awards</div>
 <ul>
 	<li>Guangdong Provience-wide Swimming Competition Youth Group 1st</li>
@@ -47,18 +50,21 @@
 	<li>Shenzhen City-wide Swimming Competition Kid Group 1st</li>
 	<li>Others</li>
 </ul>
+</div>
 
 </article>
 
 <aside class="col l4 s12">
 <h3>Street-Dancing</h3>
 <p>Zipei started street-dancing when he was 9th grade.</p>
+<div  class="home-s2-li">
 <div class="home-s2-div">Dancing Team</div>
 <ul>
 	<li>SZSY Shadow Dancing Crew</li>
 	<li>SMS Nightfury Crew</li>
 	<li>Wabble-Street</li>
 </ul>
+</div>
 </aside>
 
 
---
+++ pf.php
@@ -0,0 +1,98 @@
+<?php include "inc/topPart.inc"; ?>
+
+<?php include "inc/nav.inc"; ?>
+
+<div class="carousel carousel-slider" data-indicators="true">
+<div class="carousel-fixed-item center">
+      <a href="http://www.urcsc174.org/zyang35/" class="btn waves-effect grey white-text darken-text-2">Zipei's Work Index</a>
+    </div>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC170_Project%201/"><img src="images/p1.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC170_Project%202/"><img src="images/p2.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC170_Project%203/"><img src="images/p3.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC174_Project%201/"><img src="images/p4.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC174_Project%202/"><img src="images/p5.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC174_Lab%209/"><img src="images/L1.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/lab11/"><img src="images/L2.png"></a>
+    <a class="carousel-item" href="http://www.urcsc174.org/zyang35/CSC174_Lab%206/"><img src="images/L3.png"></a>
+</div>
+
+
+  		<div class="section ">
+    	<div class="row pf-s2">
+             <h2>Accomplishment</h2>
+			 <h3>Website List</h3>
+
+
+          <div class="inline1">
+
+
+          <div class="s2-border">
+		<img src="images/p1-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC170_Project%201/">CSC170-Project 1</a>
+		<p><b>Description:</b> Project about plain HTML done in CSC170</p>
+
+
+              </div><!--.colophon-border-->
+
+              <div class="s2-border">
+		<img src="images/p2-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC170_Project%202/">CSC170-Project 2</a>
+		<p><b>Description:</b> Project about CSS done in CSC170</p>
+                 </div><!--.colophon-border-->
+         </div>
+
+<div class="inline1">
+         <div class="s2-border">
+		<img src="images/p3-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC170_Project%203/">CSC170-Project 3</a>
+		<p><b>Description:</b> Project about MobileFriendly done in CSC170</p>
+		</div><!--.colophon-border-->
+
+	    <div class="s2-border">
+		<img src="images/p4-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC174_Project%201/">CSC174-Project 1</a>
+		<p><b>Description:</b> Project about Framework done in CSC174</p>
+		</div><!--.colophon-border-->
+</div>
+
+<div class="inline1">
+		<div class="s2-border">
+		<img src="images/p5-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC174_Project%202/">CSC174-Project 2</a>
+		<p><b>Description:</b> Project about Framework Customization done in CSC174</p>
+		</div><!--.colophon-border-->
+
+		<div class="s2-border">
+		<img src="images/L1-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC174_Lab%209/">CSC174-Lab 9</a>
+		<p><b>Description:</b> Lab about Long-scrolly Webpage done in CSC174</p>
+		</div><!--.colophon-border-->
+</div>
+
+<div class="inline1">
+
+		<div class="s2-border">
+		<img src="images/L2-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/lab11/">CSC174-Lab 11</a>
+		<p><b>Description:</b> Lab about WordPress done in CSC174</p>
+		</div><!--.colophon-border-->
+
+		<div class="s2-border">
+		<img src="images/L3-2.png">
+		<br><br><a href="http://www.urcsc174.org/zyang35/CSC174_Lab%206/">CSC174-Lab 6</a>
+		<p><b>Description:</b> Lab about CSS Layout done in CSC174</p>
+		</div><!--.colophon-border-->
+
+						     
+
+               </div><!--.inline1-->
+</div><!--row container -->
+  		</div><!--section -->
+      </div><!--container -->
+
+
+
+<?php include "inc/scripts.inc" ?>
+
+ </body>
+</html>
